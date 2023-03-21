dcc74e1e12bf5ab59d7e8a94e6ff3b9c1e628cb8
Daniel <skap.tk@gmail.com>
2023-03-21 17:29:09 -0500

    Base project added

---
+++ src/main/java/com/sykp/statsapiservice/StatsApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sykp.statsapiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatsApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatsApiServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sykp/statsapiservice/StatsApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sykp.statsapiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatsApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
